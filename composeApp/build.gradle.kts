ed0183841769b717d3a89c370216f9286ba067cd
flug8 <flug0088@gmx.ch>
2026-03-17 14:04:49 +0100

    added different package name for debug app

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -89,6 +89,10 @@ android {
         getByName("release") {
             isMinifyEnabled = false
         }
+        getByName("debug") {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
